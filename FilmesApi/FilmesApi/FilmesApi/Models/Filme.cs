ad79b748583f7309fd6d26700834ec6a17d7c878
André Macáu <andre.macau83@hotmail.com>
2023-04-25 16:41:47 -0300

    Validações

--- FilmesApi/FilmesApi/FilmesApi/Models/Filme.cs
+++ FilmesApi/FilmesApi/FilmesApi/Models/Filme.cs
@@ -1,8 +1,15 @@
-﻿namespace FilmesApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
 
 public class Filme
 {
+    [Required(ErrorMessage ="O título do Filme é obrigatório!")]
     public string Titulo { get; set; }
+    [Required(ErrorMessage ="O gênero do Filme é obrigatório!")]
+    [MaxLength(50, ErrorMessage = "O Tamanho do gênero não pode exeder 50 caracteres!")]
     public string Genero { get; set; }
+    [Required]
+    [Range(70, 600, ErrorMessage ="A duração dever ter entre 70 e 600 minutos")]
     public int Duracao { get; set; }
 }
